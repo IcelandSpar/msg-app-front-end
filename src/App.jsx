f6192decbc3bf308f04065e78f5bfe6f16c50c40
IcySpar <victorasielrodriguez@gmail.com>
2025-08-12 09:54:29 -0700

    build: test socket and initialize socket.io client
    testing cors
    test http req can be made as well

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import { socket } from './socket';
+import { useState, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const btnHandler = (e) => {
+    e.preventDefault();
+    socket.emit('button press');
+  
+  }
+
+  useEffect(() => {
+
+    socket.on('connection', (msg) => {
+      console.log(msg)
+    })
+
+    socket.on('button press', (msg) => {
+      console.log(msg)
+    });
+
+    socket.on('user left', (msg) => {
+      console.log(msg)
+    })
+
+    fetch(`http://localhost:3000`, {method: 'GET'})
+    .then((res) => res.json())
+    .then((res) => console.log(res))
+    .catch((err) => console.error(err))
+  }, []);
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={btnHandler}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
