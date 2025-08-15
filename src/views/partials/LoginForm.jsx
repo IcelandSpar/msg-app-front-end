5bd3a9d0a6e57d0861f989e23ce9b460eaff4bed
IcySpar <victorasielrodriguez@gmail.com>
2025-08-15 12:23:04 -0700

    feat: add login and register msgs and redirect
    login will set jwt to client localStorage

--- src/views/partials/LoginForm.jsx
+++ src/views/partials/LoginForm.jsx
@@ -1,11 +1,17 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
+import { useNavigate } from 'react-router';
 
 const LoginForm = () => {
+  const [ loginErr, setLoginErr ] = useState(null);
+
   const usernameInput = useRef(null);
   const passwordRef = useRef(null);
 
+  const navigate = useNavigate();
+
 const handleSubmit = (e) => {
     e.preventDefault();
+    setLoginErr(null);
     const formData = new FormData();
     formData.append('username', usernameInput.current.value);
     formData.append('password', passwordRef.current.value);
@@ -15,13 +21,26 @@ const handleSubmit = (e) => {
       body: new URLSearchParams(formData),
     })
     .then((res) => res.json())
-    .then((res) => console.log(res))
+    .then((res) => {
+      if(res.token) {
+        localStorage.setItem('msgAppToken', res.token);
+        navigate('/')
+      } else if (res.message) {
+        setLoginErr(res.message);
+      }
+    })
     .catch(err => console.error(err))
   }
 
 
   return (
     <form onSubmit={handleSubmit}>
+      { !loginErr ?  null : (
+        <div>
+          <h3>Please Fix:</h3>
+          <p>{loginErr}</p>
+        </div>
+      )}
       <div>
         <label htmlFor="username">Username:</label>
         <input type="text" id="username" name="username" ref={usernameInput} />
@@ -30,7 +49,7 @@ const handleSubmit = (e) => {
         <label htmlFor="password">Password:</label>
         <input type="text" id="password" name="password" ref={passwordRef} />
       </div>
-      <button onClick={handleSubmit} type="submit">
+      <button type="submit">
         Submit
       </button>
     </form>
--- src/views/partials/RegisterForm.jsx
+++ src/views/partials/RegisterForm.jsx
@@ -1,10 +1,16 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
+import { useNavigate } from 'react-router';
 
 const RegisterForm = () => {
+  const [ registerErr, setRegisterErr ] = useState(null);
+
   const usernameInput = useRef(null);
   const passwordRef = useRef(null);
+  
+  const navigate = useNavigate();
 
   const handleSubmit = (e) => {
+    setRegisterErr(null);
     e.preventDefault();
     const formData = new FormData();
     formData.append('username', usernameInput.current.value);
@@ -15,13 +21,28 @@ const RegisterForm = () => {
       body: new URLSearchParams(formData),
     })
     .then((res) => res.json())
-    .then((res) => console.log(res))
-    .catch(err => console.error(err))
+    .then((res) => {
+      if(res.message) {
+        setRegisterErr(res.message);
+        return res
+      } else if(res.success) {
+        navigate('/login')
+      }
+    })
+    .catch(err => {
+      console.error(err);
+    })
   }
 
 
   return (
-    <form onSubmit={handleSubmit} action={`${import.meta.env.VITE_FETCH_BASE_URL}/register`} method="POST">
+    <form onSubmit={handleSubmit} action={`${import.meta.env.VITE_FETCH_BASE_URL}/register`}>
+      {!registerErr ? null : (
+        <div>
+          <h3>Please fix:</h3>
+          <p>{registerErr}</p>
+        </div>
+      )}
       <div>
         <label htmlFor="username">Username:</label>
         <input type="text" id="username" name="username" ref={usernameInput}/>
@@ -30,7 +51,7 @@ const RegisterForm = () => {
         <label htmlFor="password">Password:</label>
         <input type="text" id="password" name="password" ref={passwordRef}/>
       </div>
-      <button onClick={handleSubmit} type="submit">Submit</button>
+      <button type="submit">Submit</button>
     </form>
   );
 };
