6a5d43d159d2900e0f2da58a2a53d13d69cc582c
IcySpar <victorasielrodriguez@gmail.com>
2025-10-07 22:22:36 -0700

    feat: add styles/css to addFriendModal and Icon

--- src/views/partials/AddFriendModal.jsx
+++ src/views/partials/AddFriendModal.jsx
@@ -1,35 +1,53 @@
-import { useContext, useRef } from 'react';
+import { useContext, useRef } from "react";
 
-import UserContext from '../../UserContext';
+import UserContext from "../../UserContext";
 
-import styles from '../../styles/AddFriendModal.module.css';
+import styles from "../../styles/AddFriendModal.module.css";
+import wavingHandIcon from "../../assets/waving_hand_icon.svg";
 
-const AddFriendModal = ({handleFriendReqSubmit, handleCloseFriendModal}) => {
+const AddFriendModal = ({ handleFriendReqSubmit, handleCloseFriendModal }) => {
   const friendCodeInput = useRef(null);
 
   const { profile } = useContext(UserContext);
 
-
-
-
   return (
     <div className={styles.modalBackground}>
       <div className={styles.modalCont}>
-        <h3>Add a friend</h3>
-        <form onSubmit={(e) => handleFriendReqSubmit(e, friendCodeInput)}>
-          <fieldset>
-            <legend>Send a Friend Request</legend>
+        <button
+          className={styles.exitModalBtn}
+          onClick={handleCloseFriendModal}
+          type="button"
+        >
+          X
+        </button>
+
+        <form className={styles.friendReqForm} onSubmit={(e) => handleFriendReqSubmit(e, friendCodeInput)}>
+          <div className={styles.modalHeadingAndParaCont}>
+            <h3 className={styles.modalHeading}>Send a Friend Request!</h3>
+            <p className={styles.modalPara}>You need someone's friend code</p>
+          </div>
+          <fieldset className={styles.friendReqFieldset}>
             <div className={styles.labelAndInputCont}>
               <label htmlFor="friendCode">Friend Code</label>
-              <input ref={friendCodeInput} type="text" name="friendCode" id="friendCode" />
+              <input
+              className={styles.friendCodeInput}
+                ref={friendCodeInput}
+                type="text"
+                name="friendCode"
+                id="friendCode"
+              />
             </div>
           </fieldset>
-          <button type='submit'>Send Request</button>
-          <button onClick={handleCloseFriendModal} type='button'>Exit</button>
+          <div className={styles.sendReqBtnCont}>
+           <button className={styles.sendReqBtn} type="submit">
+            <p className={styles.sendReqPara}>Send Request</p>
+            <img className={styles.sendReqIcon} src={wavingHandIcon} alt="Send friend request" />
+           </button>
+          </div>
         </form>
       </div>
     </div>
-  )
+  );
 };
 
 export default AddFriendModal;
