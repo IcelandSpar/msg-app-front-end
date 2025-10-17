a169aaf75726c51b149d778364274a9a1a44c5ac
IcySpar <victorasielrodriguez@gmail.com>
2025-10-16 22:32:09 -0700

    feat: add css styles to groupChatMain Components

--- src/views/partials/MsgForm.jsx
+++ src/views/partials/MsgForm.jsx
@@ -63,65 +63,71 @@ const MsgForm = ({ setChatMsgs, endOfMsg }) => {
         });
       })
       .catch((err) => console.error(err))
-      .finally(() => {
-      });
+      .finally(() => {});
 
-      setTimeout(() => {
-      endOfMsg.current?.scrollIntoView({ behavior: 'smooth' });
+    setTimeout(() => {
+      endOfMsg.current?.scrollIntoView({ behavior: "smooth" });
       messageInput.current.value = "";
-
-      }, 1000)
-
+    }, 1000);
   };
-
 
   useEffect(() => {
     let typingTimer;
 
     socket.on("user typing", ({ profileName, typerGroupId }) => {
-      if(typerGroupId == groupId) {
-      clearTimeout(typingTimer);
-      setTypingUserObj({
-        isUserTyping: true,
-        typingUser: profileName,
-        groupId: typerGroupId,
-      });
+      if (typerGroupId == groupId) {
+        clearTimeout(typingTimer);
+        setTypingUserObj({
+          isUserTyping: true,
+          typingUser: profileName,
+          groupId: typerGroupId,
+        });
 
-    typingTimer = setTimeout(() => {
-      setTypingUserObj(null);
-    }, 1000);
+        typingTimer = setTimeout(() => {
+          setTypingUserObj(null);
+        }, 1000);
       }
-
     });
 
     return () => {
       socket.off("user typing");
-    }
-
+    };
   }, []);
 
   return (
     <form onSubmit={handleSubmitMsgForm} className={styles.msgForm}>
-      {!typingUserObj || typingUserObj.isUserTyping == false ? null : (
-        <div className={styles.userTypingCont}>
-          <p>{typingUserObj.typingUser} is typing...</p>
-        </div>
-      )}
       <div className={styles.inputAndBtnCont}>
-        <div>
-          <label className={styles.msgLabelHidden} htmlFor="message">Message: </label>
-          <textarea
-            className={styles.msgTextarea}
-            onChange={handleKeyUp}
-            ref={messageInput}
-            name="message"
-            id="message"
-            placeholder="Type a message!"
-            rows={3}
-            cols={50}
-          ></textarea>
+        <div className={styles.userTypingAndMsgTextareaCont}>
+          <label className={styles.msgLabelHidden} htmlFor="message">
+            Message:{" "}
+          </label>
+          {!typingUserObj || typingUserObj.isUserTyping == false ? null : (
+            <div className={styles.userTypingCont}>
+              <p>{typingUserObj.typingUser} is typing...</p>
+            </div>
+          )}
+          <div className={styles.textAreaMsgBtnCont}>
+            <textarea
+              className={styles.msgTextarea}
+              onChange={handleKeyUp}
+              ref={messageInput}
+              name="message"
+              id="message"
+              placeholder="Type a message!"
+              rows={3}
+              cols={50}
+            ></textarea>
+            <button className={styles.msgFormSendBtn} type="submit">
+              <img
+                className={styles.sendMsgIcon}
+                src={sendMsgIcon}
+                alt="send message"
+                width={"25px"}
+                height={"25px"}
+              />
+            </button>
+          </div>
         </div>
-        <button className={styles.msgFormSendBtn} type="submit"><img className={styles.sendMsgIcon} src={sendMsgIcon} alt="send message" width={'25px'} height={'25px'}/></button>
       </div>
     </form>
   );
