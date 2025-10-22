7f6de8a20fcf7565b0fe20dca84aac5fef96b124
IcySpar <victorasielrodriguez@gmail.com>
2025-10-21 22:57:44 -0700

    feat: add css/styles to home page

--- src/views/DefaultHome.jsx
+++ src/views/DefaultHome.jsx
@@ -1,13 +1,24 @@
 import Navbar from './partials/Navbar.jsx';
 
 import styles from '../styles/DefaultHome.module.css';
+import groupTableImg from "../assets/group_chat_home_art.svg";
+import msgInBottleIcon from "../assets/msg_in_bottle.svg";
+
 
 const DefaultHome = () => {
   return (
     <div className={styles.defaultHomePage}>
     <Navbar/>
-    <main>
-    <h1>This is the home page</h1>
+    <main className={styles.defaultHomeMainCont}>
+      <div className={styles.imgAndParaCont}>
+        <p className={styles.imgDescriptionPara}>Send Messages instantly, say goodbye to waiting!</p>
+        <a className={styles.anchorTagCont} href="https://pixabay.com/users/skorec-16694100/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=7048053"><img className={styles.msgInBottleIcon} src={msgInBottleIcon} alt="Bottles in water with messages" width={'300px'} height={'300px'}/></a>
+      </div>
+      <div className={`${styles.imgAndParaCont}`}>
+        <a className={styles.anchorTagCont}  href={"https://pixabay.com/users/poli_-4882926/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=8320174"}><img src={groupTableImg} alt="Group talking" width={'300px'} height={'300px'}/></a>
+        <p className={styles.imgDescriptionPara}>Create group chats for groups of friends!</p>
+      </div>
+
     </main>
     </div>
   )
