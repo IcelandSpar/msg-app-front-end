63d8f4c04bff555b8f436b080e35e723d3bfd21d
IcySpar <victorasielrodriguez@gmail.com>
2025-10-27 21:16:51 -0700

    feat: add link to member list profilePictures

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,5 @@ import App from '../src/App.jsx'
 
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
     <App/>
-  </StrictMode>,
 )
--- src/views/partials/GroupMembersList.jsx
+++ src/views/partials/GroupMembersList.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router";
+
 import styles from "../../styles/GroupMembersList.module.css";
 import crownIcon from "../../assets/crown_icon.svg";
 
@@ -9,17 +11,23 @@ const GroupMembersList = ({ groupMembers }) => {
         {groupMembers.adminRoleMembers.map((admin, indx) => {
           return (
             <li className={styles.memberLis}>
-              <img
-                className={styles.memberProfileImg}
-                src={`${import.meta.env.VITE_FETCH_BASE_URL}/${
-                  admin.member.profileImgFilePath
-                }`}
-                alt={`${admin.member.profileName}'s profile picture`}
-                width={"25px"}
-                height={"25px"}
-              />
+              <Link to={`/profile/${admin.member.id}`}>
+                <img
+                  className={styles.memberProfileImg}
+                  src={`${import.meta.env.VITE_FETCH_BASE_URL}/${
+                    admin.member.profileImgFilePath
+                  }`}
+                  alt={`${admin.member.profileName}'s profile picture`}
+                  width={"25px"}
+                  height={"25px"}
+                />
+              </Link>
               <p>{admin.member.profileName}</p>
-              <img className={styles.adminCrownIcon} src={crownIcon} alt="crown" />
+              <img
+                className={styles.adminCrownIcon}
+                src={crownIcon}
+                alt="crown"
+              />
             </li>
           );
         })}
@@ -29,15 +37,18 @@ const GroupMembersList = ({ groupMembers }) => {
         {groupMembers.userRoleMembers.map((member, indx) => {
           return (
             <li className={styles.memberLis} key={member.member.id}>
-              <img
-                className={styles.memberProfileImg}
-                src={`${import.meta.env.VITE_FETCH_BASE_URL}/${
-                  member.member.profileImgFilePath
-                }`}
-                alt={`${member.member.profileName}'s profile picture`}
-                width={"25px"}
-                height={"25px"}
-              />
+              <Link to={`/profile/${member.member.id}`}>
+                <img
+                  className={styles.memberProfileImg}
+                  src={`${import.meta.env.VITE_FETCH_BASE_URL}/${
+                    member.member.profileImgFilePath
+                  }`}
+                  alt={`${member.member.profileName}'s profile picture`}
+                  width={"25px"}
+                  height={"25px"}
+                />
+              </Link>
+
               <p>{member.member.profileName}</p>
             </li>
           );
